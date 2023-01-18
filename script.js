2a38ae23105afe36efbcdf7b3714fabc6b8bc077
rakmate9812 <rakmate9812@gmail.com>
2023-01-18 07:10:11 +0100

    adding-tab-navigation-skills

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Decided to skip OOP because of this project's small size
 // console.log($);
 
+// Selectors
+const skillTab = document.querySelector(".skill__container");
+const tabs = document.querySelectorAll(".nav__tab");
+console.log(tabs);
+// ///////////////////////////////////////////////////////////////////
+
+// Tabbed skill component
+skillTab.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".nav-item");
+  // console.log(clicked);
+
+  if (!clicked) return;
+
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  // TODO - add the active class, and prevent default
+  clicked.classList.add("");
+});
+// Sortable cards
 $(".card-deck-sortable").sortable({
   connectWith: ".carddeck",
 });
