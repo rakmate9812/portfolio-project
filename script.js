4ab5986bc8d30553c59d598cb04cb5cd89bf7122
rakmate9812 <rakmate9812@gmail.com>
2023-01-18 17:57:51 +0100

    scripting-skill-tab-component

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 // Decided to skip OOP because of this project's small size
+"use strict";
 // console.log($);
 
 // Selectors
-const skillTab = document.querySelector(".skill__container");
-const tabs = document.querySelectorAll(".nav__tab");
-console.log(tabs);
+const skillTab = document.querySelector(".skill__tabs__container");
+const tabs = document.querySelectorAll(".skill__tab");
+const skillDeck = document.querySelector(".skill__deck__container");
+const decks = document.querySelectorAll(".skill__deck");
 // ///////////////////////////////////////////////////////////////////
 
-// Tabbed skill component
+// Tabulated "Skills" component
 skillTab.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".nav-item");
-  // console.log(clicked);
+  e.preventDefault();
+  const clicked = e.target.closest(".skill__tab");
 
   if (!clicked) return;
 
   tabs.forEach((tab) => {
     tab.classList.remove("active");
+
+    decks[+tab.dataset.tab - 1].classList.contains("d-none")
+      ? ""
+      : decks[+tab.dataset.tab - 1].classList.add("d-none");
   });
-  // TODO - add the active class, and prevent default
-  clicked.classList.add("");
+
+  clicked.classList.add("active");
+  decks[+clicked.dataset.tab - 1].classList.remove("d-none");
 });
-// Sortable cards
+
+// Sortable skill cards
 $(".card-deck-sortable").sortable({
   connectWith: ".carddeck",
 });
